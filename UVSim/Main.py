05e48bee93d23ac55523e1e628cc1d45cf5d2db3
Jan Chico <Janchico01@gmail.com>
2026-02-06 21:27:30 -0700

    Added some unit test to tests.py

--- UVSim/Main.py
+++ UVSim/Main.py
@@ -1,54 +1,58 @@
-from email.headerregistry import Address
 from Operators import *
 #Global dictionary for the memory
-memory = [0]*100
-accumulator = 0 
-pc = 0
-halted = False
-list_of_lines = []
-File = input("Enter the file name: ")
-with open (File, "r") as file:
-    for line in file:
-        list_of_lines.append(line)
-print(list_of_lines)
 
-while pc < len(list_of_lines) and not halted:
-    line = list_of_lines[pc].strip()
-    opcode = line[1:3]
-    address = int(line[3:5])
-    match opcode:
-        case "10":
-            READ(address, memory)
-        case "11":
-            WRITE(address, memory)
-        case "20":
-            accumulator = LOAD(address, memory)
-        case "21":
-            STORE(address, memory, accumulator)
-        case "30":
-            accumulator = ADD(address, memory, accumulator)
-        case "31":
-            accumulator = SUBTRACT(address, memory,accumulator)
-        case "32":
-            accumulator = DIVIDE(address, memory,accumulator)
-        case "33":
-            accumulator = MULTIPLY(address, memory,accumulator)
-        case "40":
-            BRANCH(address)
-            continue
-        case "41":
-            new_pc = BRANCHNEG(address, accumulator)
-            if new_pc is not None:
-                pc = new_pc
-                continue
-        case "42":
-            new_pc = BRANCHZERO(address, accumulator)
-            if new_pc is not None:
-                pc = new_pc
-                continue
-        case "43":
-            halted = HALT()
-        case _:
-            print("Invalid operator")
+def main():
+    memory = [0]*100
+    accumulator = 0 
+    pc = 0
+    halted = False
+    list_of_lines = []
+    File = input("Enter the file name: ")
+    with open (File, "r") as file:
+        for line in file:
+            list_of_lines.append(line)
+    print(list_of_lines)
 
-    pc += 1
+    while pc < len(list_of_lines) and not halted:
+        line = list_of_lines[pc].strip()
+        opcode = line[1:3]
+        address = int(line[3:5])
+        match opcode:
+            case "10":
+                READ(address, memory)
+            case "11":
+                WRITE(address, memory)
+            case "20":
+                accumulator = LOAD(address, memory)
+            case "21":
+                STORE(address, memory, accumulator)
+            case "30":
+                accumulator = ADD(address, memory, accumulator)
+            case "31":
+                accumulator = SUBTRACT(address, memory,accumulator)
+            case "32":
+                accumulator = DIVIDE(address, memory,accumulator)
+            case "33":
+                accumulator = MULTIPLY(address, memory,accumulator)
+            case "40":
+                BRANCH(address)
+                continue
+            case "41":
+                new_pc = BRANCHNEG(address, accumulator)
+                if new_pc is not None:
+                    pc = new_pc
+                    continue
+            case "42":
+                new_pc = BRANCHZERO(address, accumulator)
+                if new_pc is not None:
+                    pc = new_pc
+                    continue
+            case "43":
+                halted = HALT()
+            case _:
+                print("Invalid operator")
+
+        pc += 1
+
+if __name__ == "__main__":
+    main()
--- UVSim/Tests.py
+++ UVSim/Tests.py
@@ -1,1 +1,166 @@
-import Main, Operators
+from pytest import MonkeyPatch,raises
+# from Main import *
+from Operators import *
+# import Unittest
+
+
+
+def test_read(monkeypatch):
+    memory = [0] *100
+        
+    monkeypatch.setattr('builtins.input', lambda _: '1234')
+
+    READ(5, memory)
+
+    assert memory[5] == 1234
+
+def test_read_invalid(monkeypatch):
+    memory = [0] *100
+        
+    monkeypatch.setattr('builtins.input', lambda _: 'abc')
+
+    with raises(ValueError):
+        READ(5, memory)
+
+def test_write(monkeypatch,capsys):
+    memory = [0] *100
+        
+    monkeypatch.setattr('builtins.input', lambda _: '1234')
+
+    READ(5, memory)
+
+    WRITE(5, memory)
+    printed = capsys.readouterr()
+    
+    assert printed.out == "1234\n"
+
+def test_write_fail(monkeypatch,capsys):
+    memory = [0] *100
+        
+    monkeypatch.setattr('builtins.input', lambda _: '1234')
+
+    READ(5, memory)
+
+    with raises(IndexError):
+        WRITE(200, memory)
+
+def test_load(monkeypatch):
+    memory = [0] *100
+        
+    monkeypatch.setattr('builtins.input', lambda _: '1234')
+
+    READ(5, memory)
+
+    assert LOAD(5, memory) == 1234
+
+def test_load_fail(monkeypatch):
+    memory = [0] *100
+        
+    monkeypatch.setattr('builtins.input', lambda _: '1234')
+
+    READ(5, memory)
+
+    with raises(IndexError):
+        LOAD(150, memory)
+
+def test_store(monkeypatch):
+    memory = [0] *100
+
+    accumulator = 1234
+       
+    STORE(5, memory, accumulator)
+    
+    assert memory[5] == 1234
+
+
+def test_store_invaild(monkeypatch):
+    memory = [0] *100
+
+    accumulator = "abc"
+       
+    store = STORE(5, memory, accumulator)
+    
+    def invalid_data(data):
+        if not isinstance(data, int):
+            raise TypeError("Data must be an Int")
+     
+    with raises(TypeError, match="Data must be an Int"):
+        invalid_data(memory[5])
+
+def test_add(monkeypatch):
+    memory = [0] *100
+
+    accumulator = 1234
+       
+    memory[5] = 1234
+    
+    
+    assert ADD(5, memory, accumulator) == 2468
+
+def test_add_fail(monkeypatch):
+    memory = [0] *100
+
+    accumulator = 1234
+       
+    memory[5] = 1234
+    
+    
+    with raises(IndexError):
+        ADD(200, memory, accumulator)
+
+def test_subtract():
+    memory = [0] *100
+
+    accumulator = 1234
+       
+    memory[5] = 1000
+
+    assert SUBTRACT(5, memory, accumulator) == 234
+
+def test_subtract_fail():
+    memory = [0] *100
+
+    accumulator = 1234
+       
+    memory[5] = 1000
+
+    with raises(IndexError):
+        SUBTRACT(150, memory, accumulator)
+
+def test_muliply():
+    memory = [0] *100
+
+    accumulator = 1234
+       
+    memory[5] = 2
+
+    assert MULTIPLY(5, memory, accumulator) == 2468
+
+def test_multiply_fail():
+    memory = [0] *100
+
+    accumulator = 1234
+       
+    memory[5] = 2
+
+    with raises(IndexError):
+        MULTIPLY(150, memory, accumulator)
+
+def test_divide():
+    memory = [0] *100
+
+    accumulator = 1234
+       
+    memory[5] = 2
+
+    assert DIVIDE(5, memory, accumulator) == 617
+
+def test_divide_fail():
+    memory = [0] *100
+
+    accumulator = 1234
+       
+    memory[5] = 2
+
+    with raises(IndexError):
+        DIVIDE(150, memory, accumulator)
