30e26ff89dcc62b62f10cd1184979d6e70f4dbbf
Jan Chico <Janchico01@gmail.com>
2026-02-21 13:10:14 -0700

    Made operators.py into a class, and implemented it into simulator.py

--- UVSim/Operators.py
+++ UVSim/Operators.py
@@ -1,56 +1,57 @@
-#I/O operation:
-#READ = 10 Read a word from the keyboard into a specific location in memory.
+class Operators:
+    #I/O operation:
+    #READ = 10 Read a word from the keyboard into a specific location in memory.
 
-def READ(location, memory):
-    toStore = int(input("Enter a word: "))
-    if -9999 <= toStore <= 9999:
-        memory[int(location)] = toStore
-        return memory
-    else:
-        raise ValueError("Input must be between, -9999 and 9999.")
-#WRITE = 11 Write a word from a specific location in memory to screen.
-def WRITE(location, memory):
-    print(memory[int(location)])
-#Load/store operations:
-#LOAD = 20 Load a word from a specific location in memory into the accumulator.
-def LOAD(location, memory):
-    return memory[int(location)]
-#STORE = 21 Store a word from the accumulator into a specific location in memory.
-def STORE(location, memory, accumulator):
-    memory[int(location)] = accumulator 
+    def READ(self, location, memory):
+        toStore = int(input("Enter a word: "))
+        if -9999 <= toStore <= 9999:
+            memory[int(location)] = toStore
+            return memory
+        else:
+            raise ValueError("Input must be between, -9999 and 9999.")
+    #WRITE = 11 Write a word from a specific location in memory to screen.
+    def WRITE(self, location, memory):
+        print(memory[int(location)])
+    #Load/store operations:
+    #LOAD = 20 Load a word from a specific location in memory into the accumulator.
+    def LOAD(self, location, memory):
+        return memory[int(location)]
+    #STORE = 21 Store a word from the accumulator into a specific location in memory.
+    def STORE(self, location, memory, accumulator):
+        memory[int(location)] = accumulator 
 
-#Arithmetic operation:
-#ADD = 30 Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)
-def ADD(location, memory, accumulator):
-    accumulator = accumulator + memory[int(location)]
-    return accumulator;
-#SUBTRACT = 31 Subtract a word from a specific location in memory from the word in the accumulator (leave the result in the accumulator)
-def SUBTRACT(location, memory, accumulator):
-    accumulator = accumulator - memory[int(location)]
-    return accumulator;
-#DIVIDE = 32 Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator).
-def DIVIDE(location, memory, accumulator):
-    accumulator = accumulator // memory[int(location)]
-    return accumulator;
-#MULTIPLY = 33 multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator).
-def MULTIPLY(location, memory, accumulator):
-    accumulator = accumulator * memory[int(location)]
-    return accumulator;
+    #Arithmetic operation:
+    #ADD = 30 Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)
+    def ADD(self, location, memory, accumulator):
+        accumulator = accumulator + memory[int(location)]
+        return accumulator;
+    #SUBTRACT = 31 Subtract a word from a specific location in memory from the word in the accumulator (leave the result in the accumulator)
+    def SUBTRACT(self, location, memory, accumulator):
+        accumulator = accumulator - memory[int(location)]
+        return accumulator;
+    #DIVIDE = 32 Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator).
+    def DIVIDE(self, location, memory, accumulator):
+        accumulator = accumulator // memory[int(location)]
+        return accumulator;
+    #MULTIPLY = 33 multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator).
+    def MULTIPLY(self, location, memory, accumulator):
+        accumulator = accumulator * memory[int(location)]
+        return accumulator;
 
-#Control operation:
-#BRANCH = 40 Branch to a specific location in memory
-def BRANCH(location):
-    return int(location)
-#BRANCHNEG = 41 Branch to a specific location in memory if the accumulator is negative.
-def BRANCHNEG(address, accumulator):
-    if accumulator < 0:
-        return int(address)
-    return None
-#BRANCHZERO = 42 Branch to a specific location in memory if the accumulator is zero.
-def BRANCHZERO(address, accumulator):
-    if accumulator == 0:
-        return int(address)
-    return None
-#HALT = 43 Pause the program
-def HALT():
-    return True
+    #Control operation:
+    #BRANCH = 40 Branch to a specific location in memory
+    def BRANCH(self, location):
+        return int(location)
+    #BRANCHNEG = 41 Branch to a specific location in memory if the accumulator is negative.
+    def BRANCHNEG(self, address, accumulator):
+        if accumulator < 0:
+            return int(address)
+        return None
+    #BRANCHZERO = 42 Branch to a specific location in memory if the accumulator is zero.
+    def BRANCHZERO(self, address, accumulator):
+        if accumulator == 0:
+            return int(address)
+        return None
+    #HALT = 43 Pause the program
+    def HALT(self):
+        return True
--- UVSim/Simulator.py
+++ UVSim/Simulator.py
@@ -7,51 +7,52 @@ class BasicMLSimulator:
         self.pc = 0
         self.halted = False
         self.program = []
+        self.op = Operators()
 
     def load_program(self, filename):
         with open(filename, "r") as file:
             self.program = [line.strip() for line in file]
 
     def step(self):
+        self.memory 
         if self.pc >= len(self.program) or self.halted:
             return
-
         line = self.program[self.pc]
         opcode = line[1:3]
         address = int(line[3:5])
 
         match opcode:
             case "10":
-                READ(address, self.memory)
+                self.op.READ(address, self.memory)
             case "11":
-                WRITE(address, self.memory)
+                self.op.WRITE(address, self.memory)
             case "20":
-                self.accumulator = LOAD(address, self.memory)
+                self.accumulator = self.op.LOAD(address, self.memory)
             case "21":
-                STORE(address, self.memory, self.accumulator)
+                self.op.STORE(address, self.memory, self.accumulator)
             case "30":
-                self.accumulator = ADD(address, self.memory, self.accumulator)
+                self.accumulator = self.op.ADD(address, self.memory, self.accumulator)
             case "31":
-                self.accumulator = SUBTRACT(address, self.memory, self.accumulator)
+                self.accumulator = self.op.SUBTRACT(address, self.memory, self.accumulator)
             case "32":
-                self.accumulator = DIVIDE(address, self.memory, self.accumulator)
+                self.accumulator = self.op.DIVIDE(address, self.memory, self.accumulator)
             case "33":
-                self.accumulator = MULTIPLY(address, self.memory, self.accumulator)
+                self.accumulator = self.op.MULTIPLY(address, self.memory, self.accumulator)
             case "40":
-                self.pc = BRANCH(address)
+                self.pc = self.op.BRANCH(address)
                 return
             case "41":
-                new_pc = BRANCHNEG(address, self.accumulator)
+                new_pc = self.op.BRANCHNEG(address, self.accumulator)
                 if new_pc is not None:
                     self.pc = new_pc
                     return
             case "42":
-                new_pc = BRANCHZERO(address, self.accumulator)
+                new_pc = self.op.BRANCHZERO(address, self.accumulator)
                 if new_pc is not None:
                     self.pc = new_pc
                     return
             case "43":
-                self.halted = HALT()
+                self.halted = self.op.HALT()
             case _:
                 print("Invalid operator")
 
