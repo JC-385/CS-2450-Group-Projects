4d40780743a5849dd86de753d9274b82444a90fc
Andrei Selikhov <147940475+Dudece@users.noreply.github.com>
2026-02-21 12:16:22 -0700

    Update Main.py
    Main is now clean and safe

--- UVSim/Main.py
+++ UVSim/Main.py
@@ -1,61 +1,14 @@
-from Operators import *
-from Gui import *
-#Global dictionary for the memory
+from Simulator import BasicMLSimulator
+from GUI import getEntryValue, run_gui
 
 def main():
-    memory = [0]*100
-    accumulator = 0 
-    pc = 0
-    halted = False
-    list_of_lines = []
-    File = getEntryValue("file")
-    with open (File, "r") as file:
-        for line in file:
-            list_of_lines.append(line)
-    print(list_of_lines)
+    simulator = BasicMLSimulator()
 
-    while pc < len(list_of_lines) and not halted:
-        line = list_of_lines[pc].strip()
-        opcode = line[1:3]
-        address = int(line[3:5])
-        match opcode:
-            case "10":
-                READ(address, memory)
-            case "11":
-                WRITE(address, memory)
-            case "20":
-                accumulator = LOAD(address, memory)
-            case "21":
-                STORE(address, memory, accumulator)
-            case "30":
-                accumulator = ADD(address, memory, accumulator)
-            case "31":
-                accumulator = SUBTRACT(address, memory,accumulator)
-            case "32":
-                accumulator = DIVIDE(address, memory,accumulator)
-            case "33":
-                accumulator = MULTIPLY(address, memory,accumulator)
-            case "40":
-                BRANCH(address)
-                continue
-            case "41":
-                new_pc = BRANCHNEG(address, accumulator)
-                if new_pc is not None:
-                    pc = new_pc
-                    continue
-            case "42":
-                new_pc = BRANCHZERO(address, accumulator)
-                if new_pc is not None:
-                    pc = new_pc
-                    continue
-            case "43":
-                halted = HALT()
-            case _:
-                print("Invalid operator")
+    filename = getEntryValue("file")
+    simulator.load_program(filename)
+    simulator.run()
 
-        pc += 1
-
-    run_gui()  # Keep window open after simulation (or remove to close immediately)
+    run_gui()
 
 if __name__ == "__main__":
     main()
