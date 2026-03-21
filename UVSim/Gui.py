2326b56695e3ea143ab1bb974f462395e75bfb2c
Hyrum Reynolds <hyrumreynolds@Hyrums-MacBook-Air.local>
2026-03-21 17:19:06 -0600

    Gui.py comment and instructions polish

--- UVSim/Gui.py
+++ UVSim/Gui.py
@@ -20,7 +20,7 @@ entry.pack(pady=5)
 result_var = tk.StringVar()
 current_filepath = None
 
-#this creates a small text editor where you can modify the text file with your operations
+# creates a small text editor where you can modify the text file with your operations
 text = tk.Text(root, fg='dark green', bg='white', font='times-new-roman 14', width=50, height=10)
 text.pack()
 
@@ -42,7 +42,7 @@ def getEntryValue(entryType):
     root.wait_variable(result_var)  # Blocks until result_var is set
     return result_var.get()
 
-#this allows you to open your file that you want.
+# allows you to open the file that you want.
 def open_file():
     global current_filepath
 
@@ -55,7 +55,7 @@ def open_file():
         with open(filepath, 'r') as file:
             text.delete('1.0', tk.END)
             text.insert(tk.END, file.read())
-#this saves the changes to your file.
+# saves the changes to your file.
 def save_file():
     global current_filepath
 
@@ -67,44 +67,44 @@ def save_file():
         with open(current_filepath, 'w') as file:
             file.write(text.get('1.0', tk.END))
 
-#Button - with a callback
 tk.Button(root, text="Open File", command=open_file).pack(pady = 10)
 tk.Button(root, text="Save File", command=save_file).pack(pady = 10)
 button = tk.Button(root, text="RUN", command=on_click)
 button.pack(pady=10)
 
-def second_button():
-    #this prints all the operations the program has
-    operations = [ 'I/O operation:',
-    'READ = 10 Read a word from the keyboard into a specific location in memory.',
-    'WRITE = 11 Write a word from a specific location in memory to screen.',
-    'Load/store operations:',
-    'LOAD = 20 Load a word from a specific location in memory into the accumulator.',
-    'STORE = 21 Store a word from the accumulator into a specific location in memory.',
-    'Arithmetic operation:',
-    'ADD = 30 Add a word from a specific location in memory to the word in the accumulator.',
-    'SUBTRACT = 31 Subtract a word from a specific location in memory from the word in the accumulator.',
-    'DIVIDE = 32 Divide the word in the accumulator by a word from a specific location in memory.',
-    'MULTIPLY = 33 multiply a word from a specific location in memory to the word in the accumulator.',
-    'Control operation:',
-    'BRANCH = 40 Branch to a specific location in memory',
-    'BRANCHNEG = 41 Branch to a specific location in memory if the accumulator is negative.',
-    'BRANCHZERO = 42 Branch to a specific location in memory if the accumulator is zero.',
-    'HALT = 43 Pause the program']
-    print('Program operations')
-    for i in operations:
-        print(f'{i}\n')
+def console_instructions():
+    # prints all the operations the program has
+    operations = [
+        'Program operations:',
+        'I/O operation:',
+        '   READ = 10 Read a word from the keyboard into a specific location in memory.',
+        '   WRITE = 11 Write a word from a specific location in memory to screen.',
+        '   Load/store operations:',
+        '   LOAD = 20 Load a word from a specific location in memory into the accumulator.',
+        '   STORE = 21 Store a word from the accumulator into a specific location in memory.',
+        'Arithmetic operation:',
+        '   ADD = 30 Add a word from a specific location in memory to the word in the accumulator.',
+        '   SUBTRACT = 31 Subtract a word from a specific location in memory from the word in the accumulator.',
+        '   DIVIDE = 32 Divide the word in the accumulator by a word from a specific location in memory.',
+        '   MULTIPLY = 33 multiply a word from a specific location in memory to the word in the accumulator.',
+        'Control operation:',
+        '   BRANCH = 40 Branch to a specific location in memory',
+        '   BRANCHNEG = 41 Branch to a specific location in memory if the accumulator is negative.',
+        '   BRANCHZERO = 42 Branch to a specific location in memory if the accumulator is zero.',
+        '   HALT = 43 Pause the program'
+    ]
+    print(*operations, sep="\n\n")
 
 
 
-button2 = tk.Button(root, text="Operation Instructions", command=second_button)
+button2 = tk.Button(root, text="Operation Instructions", command=console_instructions)
 button2.pack(pady = 10)
 
-def third_button():
-    #this operation will close the app.
+def close():
+    # closes the app.
     root.destroy()
 
-close_button = tk.Button(root, text="Exit Program", command=third_button)
+close_button = tk.Button(root, text="Exit Program", command=close)
 close_button.pack(pady=10)
 
 
